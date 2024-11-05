f3627010465437bfb3b8ec371c4f676b6887c6f1
SunJiHoon <cangogi1@naver.com>
2024-11-05 13:50:34 +0900

    스프링 프로젝트 추가

---
+++ piotgreen/src/main/java/com/piotgreen/piotgreen/PiotgreenApplication.java
@@ -0,0 +1,13 @@
+package com.piotgreen.piotgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiotgreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiotgreenApplication.class, args);
+	}
+
+}
---
+++ piotgreen/src/test/java/com/piotgreen/piotgreen/PiotgreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotgreen.piotgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiotgreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
