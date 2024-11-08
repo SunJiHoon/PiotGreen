d8610b7659eafa98a7d1d5bbe381978bf0b00a4c
kimju0 <kimuniverse0@naver.com>
2024-11-09 04:02:10 +0900

    ftr: 기본적인 틀 구성

---
+++ lighting_control/light_control.c
@@ -0,0 +1,39 @@
+#include <light_control.h>
+#include <wiringPi.h>
+#include <pthread.h>
+
+#define LEDPIN 1
+
+// server 코드에는 wiringPi관련 코드 없애고 여기로 옮기기
+// 메인서버로 조도 측정값을 실시간으로 보내기 위한 스레드 추가로 만들기
+// 공유 메모리 동기화하기 (뮤텍스), 추가 스레드 안만들어서 아직은 문제 없을 듯
+
+int LEDstatus = 0, mode = 0; // mode: 자동/수동
+int light;                   // 공유 메모리이므로 mutex 활용할 것
+pthread_mutex_t mutex;
+
+void setup()
+{
+    wiringPiSetup();
+    pinMode(LEDPIN, OUTPUT);
+}
+
+void changeMode(int t_mode, int init)
+{
+    mode = t_mode;
+    LEDstatus = init;
+}
+
+int changeLEDstatus(int t_status)
+{
+    if (mode == 1)
+        return 1;
+    LEDstatus = t_status;
+    return 0;
+}
+
+void sendLight()
+{
+    // 메인서버로 송신할 때 새로운 스레드가 사용할 함수
+    // ...
+}
---
+++ lighting_control/light_control.h
@@ -0,0 +1,9 @@
+#ifndef LIGHT_CONTROL_H
+#define LIGHT_CONTROL_H
+
+void setup();
+void changeMode();
+int changeLEDstatus();
+void sendLight();
+
+#endif
