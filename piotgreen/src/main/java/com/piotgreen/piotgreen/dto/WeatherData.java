aea4f6c0cc4dbc7538ba19423e533071349d1e9a
SunJiHoon <cangogi1@naver.com>
2024-11-29 15:52:51 +0900

    dto 추가

--- piotgreen/src/main/java/com/piotgreen/piotgreen/dto/WeatherData.java
+++ piotgreen/src/main/java/com/piotgreen/piotgreen/dto/WeatherData.java
@@ -1,4 +1,17 @@
 package com.piotgreen.piotgreen.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class WeatherData {
+    private String baseDate;
+    private String baseTime;
+    private String category;
+    private String fcstDate;
+    private String fcstTime;
+    private String fcstValue;
+    private String nx;
+    private String ny;
 }
