4ad35738788d93208c0c556054054fe76d35c47e
SunJiHoon <cangogi1@naver.com>
2024-12-01 10:33:17 +0900

    위험 메시지 송신 service 추가

---
+++ piotgreen/src/main/java/com/piotgreen/piotgreen/service/ManagerDataStorageService.java
@@ -0,0 +1,23 @@
+package com.piotgreen.piotgreen.service;
+
+import com.piotgreen.piotgreen.entity.ManagerData;
+import com.piotgreen.piotgreen.repository.ManagerDataRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ManagerDataStorageService {
+    private final SendMessageService sendMessageService;
+    private final ManagerDataRepository managerDataRepository;
+    public void sendMessageToAllManger(String message) {
+        List<ManagerData> managerDataList = managerDataRepository.findAll();
+        for (ManagerData managerData : managerDataList) {
+            sendMessageService.sendMessage(managerData.getPhoneNumber(), message);
+        }
+    }
+
+}
